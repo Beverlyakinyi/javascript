f7f64c826794a68b074c6826903e005615202cb2
Beverlyakinyi <beverlyotienoakinyi@gmail.com>
2024-03-22 16:36:17 +0300

    added files

---
+++ index.js
@@ -0,0 +1,52 @@
+// Find the last element of the following arrays.arr1 = [3,7,34,90,12]arr2 = [true, "green", "where",12,56]
+var arr1=[3,7,34,90,12];
+var c=(arr1);
+console.log('last element',arr1[arr1.length-1])
+
+
+var arr2=[true,"green","where",12,56];
+var b=(arr2);
+console.log('last element',arr2[arr2.length-1]);
+// Write a JS program
+//  that will join the following array elements into a stringmyPets = ["Cow", "Bird", "Snake", "Dog"]
+
+const myPets = ["Cow", "Bird", "Snake", "Dog"];
+let animals = myPets.toString();
+console.log({animals});
+// Write a JS script to sort the following array itemsvar arr3 = [ -5, 9, 5, 3, 2, -3, 6, 8, 4, 1 ];
+const arr3 =[-5,9,5,3,2,-3,6,8,4,1];
+let numb = arr3.sort();
+console.log({numb});
+// Write a program to remove duplicates from the following array.
+// Console the array without duplicates, and console another array that only contains the duplicates
+// let arr = ["boy", "man", "girl",            "school", "girl", "woman"];
+
+
+
+let arr=["boy","man","girl","school","girl","woman"];
+function removeDuplicates(store) {
+    return [ new Set(store)]
+    
+}
+console.log(removeDuplicates(arr));
+let all=["boy","man","girl","school","woman"]
+console.log(all)
+
+
+
+
+
+
+// Write a JS script to search for the following word in the array.
+// "food"If the word is present, return it else return "the search word was not found"let arr5 = ["the", "way", "x", 4]
+
+let arr5=["the", "way", "x"];
+
+
+
+
+
+// Write a JS script to sort the following string:let word = "renniw"
+let word =[ "renniw"];
+let h=word.sort();
+console.log({h});
